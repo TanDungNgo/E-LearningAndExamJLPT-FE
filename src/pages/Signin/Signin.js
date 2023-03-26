7d7652fac1696d35da89f9de56e946a691666c55
Tan Dung <tandung12072002@gmail.com>
2023-03-26 21:50:53 +0700

    feat: button component

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,10 +1,14 @@
 import classNames from "classnames/bind";
+import Button from "~/components/Button/Button";
 import styles from "./Signin.module.scss";
 const cx = classNames.bind(styles);
 function Signin() {
-    return (<div className={cx("title")}>
-        Login
-    </div>  );
+  return (
+    <div>
+      <Button outline> Register</Button>
+      <Button primary>Login</Button>
+    </div>
+  );
 }
 
 export default Signin;
