104e181d2430cea84f95c939cd90de5f0db06d1f
Phương Trần Khánh <102200314@sv1.dut.udn.vn>
2023-05-05 00:06:53 +0700

    fixCreateCoursePage

--- src/pages/AllCourse/AllCourse.js
+++ src/pages/AllCourse/AllCourse.js
@@ -8,69 +8,68 @@ import Footer from "~/layouts/components/Footer/Footer";
 import { faArrowLeft, faArrowRight, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import CourseCard from "~/components/CourseCard/CourseCard";
 import EnrollCard from "~/components/EnrollCard/EnrollCard";
+import { ConfigProvider, Pagination } from 'antd';
 const cx = classNames.bind(styles);
 
-function AllCourse(){
-    const [listCourse, setListCourse] = useState([
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-      ]);
-      const renderCard = () => {
-        return listCourse.map((item, index) => {
-          return (
-            <div key={index}>
-              <CourseCard />
-            </div>
-          );
-        });
-      };
+function AllCourse() {
+  const [listCourse, setListCourse] = useState([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+  ]);
+  const renderCard = () => {
+    return listCourse.map((item, index) => {
+      return (
+        <div key={index}>
+          <CourseCard />
+        </div>
+      );
+    });
+  };
   return (
     <div className={cx("container")}>
-        <div className={cx("card-img")}>
-            <img 
-                className={cx("card-img__detail")}
-                src="/images/bg_allcourse.jpg"
-                >
-            </img>
-        </div>
-        <div className={cx("card-search")}>
-            <input type="text" placeholder="Search your favourite course" required/>
-            <Button className={cx("btn-search")}>Search</Button>
-        </div>
-        <div className={cx("card-select")}>
-            <select className={cx("card-select__level")}>
-                <option value="">N5</option>
-                <option value="">N4</option>
-                <option value="">N3</option>
-            </select>
-            <select className={cx("card-select__learning")}>
-                <option value="">Kaiwa</option>
-                <option value="">Grammar</option>
-                <option value="">Kanji</option>
-                <option value="">vocabulary</option>
-                <option value="">Listening</option>
-            </select>
-        </div>
-        <div className={cx("card-course")}>
-            {renderCard()}
-        </div>
-        <div className={cx("card-pagination")}>
-            
-            <FontAwesomeIcon icon={faChevronLeft} className={cx("icon-pagination__left")}></FontAwesomeIcon>
-            <ul className={cx("ul-pagination")}>
-                <li className={cx("link")} value="1">1</li>
-                <li className={cx("link_active")} value="2">2</li>
-                <li className={cx("link")} value="3">3</li>
-                <li className={cx("link")} value="4">4</li>
-            </ul>
-            <FontAwesomeIcon icon={faChevronRight} className={cx("icon-pagination__right")}></FontAwesomeIcon>
-        </div>
+      <div className={cx("card-img")}>
+        <img
+          className={cx("card-img__detail")}
+          src="/images/bg_allcourse.jpg"
+        >
+        </img>
+      </div>
+      <div className={cx("card-search")}>
+        <input type="text" placeholder="Search your favourite course" required />
+        <Button className={cx("btn-search")}>Search</Button>
+      </div>
+      <div className={cx("card-select")}>
+        <select className={cx("card-select__level")}>
+          <option value="N5">N5</option>
+          <option value="N4">N4</option>
+          <option value="N3">N3</option>
+          <option value="N2">N2</option>
+          <option value="N1">N1</option>
+        </select>
+        <select className={cx("card-select__learning")}>
+          <option value="">Kaiwa</option>
+          <option value="">JLPT</option>
+        </select>
+      </div>
+      <div className={cx("card-course")}>
+        {renderCard()}
+      </div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#b79032",
+          },
+        }}
+      >
+        <Pagination defaultCurrent={1} total={50} className={cx("card-pagination")} />
+      </ConfigProvider>
+
     </div>
   );
 }
--- src/pages/CreateCourse/CreateCourse.js
+++ src/pages/CreateCourse/CreateCourse.js
@@ -6,80 +6,86 @@ import { Link } from "react-router-dom";
 import routes from "~/config/routes";
 import { useState } from "react";
 import { Progress } from "antd";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { UploadOutlined } from "@ant-design/icons";
 const cx = classNames.bind(styles);
 
 function CreateCourse() {
-  const [imgSrc, setImgSrc] = useState("/images/banner_course.jpg");
-  const [percent, setPercent] = useState(0);
-  const handleChangeFile = (e) => {
-    //Lấy file ra từ e
-    let file = e.target.files[0];
-    if (
-      file.type === "image/jpeg" ||
-      file.type === "image/jpg" ||
-      file.type === "image/png"
-    ) {
-      //Tạo đối tượng để đọc file
-      let reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = (e) => {
-        // console.log(e.target.result);
-        setImgSrc(e.target.result);
-        setPercent(100);
-      };
-    }
-  };
-  return (
-    <div className={cx("container")}>
-      <img className={cx("bg-create")} src="/images/bg_create-course.png" />
-      <div className={cx("card-create")}>
-        <div className={cx("card-create__left")}>
-          <div className={cx("card-create__title")}>Create a new course</div>
-          <div className="card-create__detail">
-            <input type="text" placeholder="Course Name" required />
-          </div>
-          <div className={cx("card-create__select")}>
-            <select>
-              <option selected disabled>
-                Level
-              </option>
-              <option value="N5">N5</option>
-              <option value="N4">N4</option>
-              <option value="N3">N3</option>
-            </select>
-          </div>
-          <div className="card-create__detail">
-            <input type="text" placeholder="Description" required />
-          </div>
-          <div className="card-create__detail">
-            <input type="text" placeholder="Duration" required />
-          </div>
-          <div className="card-create__detail">
-            <input type="text" placeholder="Price" required />
-          </div>
-          <Button className={cx("card-create__btn")}>Create</Button>
-        </div>
+    const [imgSrc, setImgSrc] = useState("/images/banner_course.jpg");
+    const [percent, setPercent] = useState(0);
+    const handleChangeFile = (e) => {
+        //Lấy file ra từ e
+        let file = e.target.files[0];
+        if (
+            file.type === "image/jpeg" ||
+            file.type === "image/jpg" ||
+            file.type === "image/png"
+        ) {
+            //Tạo đối tượng để đọc file
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (e) => {
+                // console.log(e.target.result);
+                setImgSrc(e.target.result);
+                setPercent(100);
+            };
+        }
+    };
+    return (
+        <div className={cx("container")}>
+            <img className={cx("bg-create")} src="/images/bg_create-course.png" />
+            <div className={cx("card-create")}>
+                <div className={cx("card-create__title")}>Create a new course</div>
+                <div className={cx("card-create__content")}>
+                    <div className={cx("card-create__left")}>
+                        <div className="card-create__detail">
+                            <input type="text" placeholder="Course Name" required />
+                        </div>
+                        <div className={cx("card-create__select")}>
+                            <select>
+                                <option selected disabled>
+                                    Level
+                                </option>
+                                <option value="N5">N5</option>
+                                <option value="N4">N4</option>
+                                <option value="N3">N3</option>
+                            </select>
+                        </div>
+                        <div className="card-create__detail">
+                            <input type="text" placeholder="Description" required />
+                        </div>
+                        <div className="card-create__detail">
+                            <input type="text" placeholder="Duration" required />
+                        </div>
+                        <div className="card-create__detail">
+                            <input type="text" placeholder="Price" required />
+                        </div>
+                    </div>
 
-        <div className={cx("card-create__right")}>
-          <div className={cx("card-create-banner")}>
-            <img className={cx("card-create-banner__img")} src={imgSrc} />
-          </div>
-          <div className={cx("card-create__import-file")}>
-              <label for="file">Choose a file</label>
-              <input
-                type="file"
-                name="file"
-                id="file"
-                className={cx("card-create__input-file")}
-                onChange={handleChangeFile}
-                accept="image/*"
-              />
-              <Progress percent={percent} />
-          </div>
+                    <div className={cx("card-create__right")}>
+                        <img className={cx("card-create__banner")} src={imgSrc} />
+                        <div className={cx("card-create__import-file")}>
+                            <label for="file">
+                                {/* <FontAwesomeIcon icon={faUpload} className={cx("icon-upload")}/> */}
+                                <UploadOutlined/>
+                                Choose a file
+                            </label>
+                            <input
+                                type="file"
+                                name="file"
+                                id="file"
+                                className={cx("card-create__input-file")}
+                                onChange={handleChangeFile}
+                                accept="image/*"
+                            />
+                            <Progress percent={percent} />
+                        </div>
+                    </div>
+                </div>
+                <Button primary className={cx("card-create__btn")}>Create</Button>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default CreateCourse;
