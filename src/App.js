4e2ea572dbcd400447868ca902afd2dcc9938e8c
Tan Dung <tandung12072002@gmail.com>
2023-05-05 23:06:42 +0700

    feat: courseService

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "./routes/routes";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {publicRoutes.map((route, index) => {
-            let Layout = Fragment;
-            if (route.layout) {
-              Layout = route.layout;
-            } else if (route.layout === null) {
-              Layout = Fragment;
-            }
-            const Page = route.component;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                }
-              ></Route>
-            );
-          })}
-          {privateRoutes.map((route, index) => {
-            let Layout = route.layout;
-            const Page = route.component;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                }
-              ></Route>
-            );
-          })}
-        </Routes>
-      </div>
-    </Router>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#b79032",
+        },
+      }}
+    >
+      <Router>
+        <div className="App">
+          <Routes>
+            {publicRoutes.map((route, index) => {
+              let Layout = Fragment;
+              if (route.layout) {
+                Layout = route.layout;
+              } else if (route.layout === null) {
+                Layout = Fragment;
+              }
+              const Page = route.component;
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  }
+                ></Route>
+              );
+            })}
+            {privateRoutes.map((route, index) => {
+              let Layout = route.layout;
+              const Page = route.component;
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <Layout>
+                      <Page />
+                    </Layout>
+                  }
+                ></Route>
+              );
+            })}
+          </Routes>
+        </div>
+      </Router>
+    </ConfigProvider>
   );
 }
 
