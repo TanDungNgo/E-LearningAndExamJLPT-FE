01074fc282817e7ec4d7da3c3dd2a45218d20635
Tan Dung <tandung12072002@gmail.com>
2023-05-07 17:24:53 +0700

    feat: api createCourse

--- src/pages/CreateCourse/CreateCourse.js
+++ src/pages/CreateCourse/CreateCourse.js
@@ -1,33 +1,63 @@
 import classNames from "classnames/bind";
 import styles from "./CreateCourse.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Button from "~/components/Button/Button";
 import { Link } from "react-router-dom";
 import routes from "~/configs/routes";
 import { useState } from "react";
-import { Progress } from "antd";
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
-import { UploadOutlined } from "@ant-design/icons";
-import {
-  Form,
-  Input,
-  Select,
-  Upload,
-  Image,
-  InputNumber,
-  Switch,
-} from "antd";
+import { Button, Progress } from "antd";
+import { ArrowLeftOutlined, UploadOutlined } from "@ant-design/icons";
+import { Form, Input, Select, Upload, Image, InputNumber, Switch } from "antd";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import storageFirebase from "~/configs/firebaseConfig";
+import courseService from "~/services/courseService";
 const cx = classNames.bind(styles);
 const { Option } = Select;
 
-const CreateCourse = ({ onSubmit }) => {
-  const [file, setFile] = useState(null);
+const CreateCourse = () => {
+  const [imgSrc, setImgSrc] = useState("/images/banner_course.jpg");
+  const [fileImage, setFileImage] = useState("");
+  const { createCourse } = courseService();
   const [progress, setProgress] = useState(0);
-  const [isSubmitting, setIsSubmitting] = useState(true);
-  const onFinish = (values) => {
-    // setIsSubmitting(true);
-    setProgress(100);
-    onSubmit(values);
+  const handleChangeFile = (e) => {
+    //Lấy file ra từ e
+    let file = e.target.files[0];
+    setFileImage(file);
+    if (
+      file.type === "image/jpeg" ||
+      file.type === "image/jpg" ||
+      file.type === "image/png"
+    ) {
+      //Tạo đối tượng để đọc file
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        // console.log(e.target.result);
+        setImgSrc(e.target.result);
+      };
+    }
+  };
+  const onFinish = async (values) => {
+    const storageRef = ref(storageFirebase, `images/${fileImage.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, fileImage);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      async () => {
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        const data = {
+          ...values,
+          banner: url,
+        };
+        console.log(data);
+        createCourse(data);
+      }
+    );
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -37,21 +67,25 @@ const CreateCourse = ({ onSubmit }) => {
     <div className={cx("container")}>
       <img className={cx("bg-create")} src="/images/bg_create-course.png" />
       <div className={cx("card-create")}>
-        <div className={cx("card-create__title")}>Create a new course</div>
-        <div className={cx("card-create__content")}>
-          <div className={cx("card-create__left")}>
-            <Form onFinish={onFinish} onFinishFailed={onFinishFailed} layout="horizontal">
-              <Form.Item className={cx("card-create__left-content")}
-                label="Name"
+        <div className={cx("card-create__header")}>
+          <label>
+            <ArrowLeftOutlined />
+            Back
+          </label>
+          <div className={cx("card-create__header-title")}>
+            Create a new course
+          </div>
+        </div>
+        <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+          <div className={cx("card-create__content")}>
+            <div className={cx("card-create__left")}>
+              <Form.Item
                 name="name"
                 rules={[{ required: true, message: "Please input a name!" }]}
               >
-                <Input 
-                  style={{marginLeft: '12%', width: '88%'}}
-                />
+                <Input placeholder="Name" />
               </Form.Item>
-              <Form.Item className={cx("card-create__left-content")}
-                label="Description"
+              <Form.Item
                 name="description"
                 rules={[
                   { required: true, message: "Please input a description!" },
@@ -61,14 +95,13 @@ const CreateCourse = ({ onSubmit }) => {
                   },
                 ]}
               >
-                <Input.TextArea />
+                <Input.TextArea placeholder="Description" />
               </Form.Item>
-              <Form.Item className={cx("card-create__left-content")}
-                label="Level"
+              <Form.Item
                 name="level"
                 rules={[{ required: true, message: "Please select a level!" }]}
               >
-                <Select style={{width: '88%', marginLeft: '13%'}}>
+                <Select placeholder="Level">
                   <Option value="N1">N1</Option>
                   <Option value="N2">N2</Option>
                   <Option value="N3">N3</Option>
@@ -76,18 +109,16 @@ const CreateCourse = ({ onSubmit }) => {
                   <Option value="N5">N5</Option>
                 </Select>
               </Form.Item>
-              <Form.Item className={cx("card-create__left-content")}
-                label="Type"
+              <Form.Item
                 name="type"
                 rules={[{ required: true, message: "Please select a type!" }]}
               >
-                <Select style={{width: '88%', marginLeft: '13%'}}>
+                <Select placeholder="Type">
                   <Option value="JLPT">JLPT</Option>
                   <Option value="Kaiwa">Kaiwa</Option>
                 </Select>
               </Form.Item>
-              <Form.Item className={cx("card-create__left-content")}
-                label="Duration"
+              <Form.Item
                 name="duration"
                 rules={[
                   {
@@ -102,12 +133,12 @@ const CreateCourse = ({ onSubmit }) => {
                 ]}
               >
                 <InputNumber
+                  placeholder="Duration"
                   addonAfter={<Form.Item noStyle>Month</Form.Item>}
-                  style={{ width: '96%', marginLeft: '5%' }}
+                  style={{ width: "100%" }}
                 />
               </Form.Item>
-              <Form.Item className={cx("card-create__left-content")}
-                label="Price"
+              <Form.Item
                 name="price"
                 rules={[
                   {
@@ -122,112 +153,44 @@ const CreateCourse = ({ onSubmit }) => {
                 ]}
               >
                 <InputNumber
-                  addonAfter={<Form.Item noStyle>VND</Form.Item>}
-                  style={{ width: '89%', marginLeft: '12%' }}
-                />
-              </Form.Item>
-            </Form>
-            {/* <div className={cx("card-create__detail")}>
-              <input
-                type="text"
-                placeholder="Course Name"
-                required
-              />
-            </div>
-            <div className={cx("card-create__select")}>
-              <Form.Item
-                label="Level"
-                name="level"
-                rules={[{ required: true, message: "Please select a level!" }]}
-              >
-                <Select>
-                  <Option value="N1">N1</Option>
-                  <Option value="N2">N2</Option>
-                  <Option value="N3">N3</Option>
-                  <Option value="N4">N4</Option>
-                  <Option value="N5">N5</Option>
-                </Select>
-              </Form.Item>
-            </div>
-            <div className={cx("card-create__detail")}>
-              <Form.Item
-                label="Description"
-                name="description"
-                rules={[
-                  { required: true, message: "Please input a description!" },
-                  {
-                    max: 100,
-                    message: "Description should be less than 100 characters!",
-                  },
-                ]}
-              >
-                <Input.TextArea />
-              </Form.Item>
-            </div>
-            <div className={cx("card-create__detail")}>
-              <Form.Item
-                label="Duration"
-                name="duration"
-                rules={[
-                  {
-                    required: true,
-                    type: "number",
-                    message: "Please input a duration!",
-                  },
-                ]}
-              >
-                <InputNumber
-                  addonAfter={<Form.Item noStyle>Month</Form.Item>}
-                  style={{ width: "90%"}}
+                  placeholder="Price"
+                  addonAfter={<Form.Item noStyle>$</Form.Item>}
+                  style={{ width: "100%" }}
                 />
               </Form.Item>
             </div>
-            <div className={cx("card-create__rangeSlider")}>
-              <Form.Item
-                label="Price"
-                name="price"
-                rules={[
-                  {
-                    required: true,
-                    type: "number",
-                    message: "Please input a price!",
-                  },
-                ]}
-              >
-                <InputNumber
-                  addonAfter={<Form.Item noStyle>VND</Form.Item>}
-                  style={{ width: "90%" }}
+            <div className={cx("card-create__right")}>
+              <img className={cx("card-create__banner")} src={imgSrc} />
+              <div className={cx("card-create__import-file")}>
+                <label htmlFor="file">
+                  <UploadOutlined />
+                  Choose a file
+                </label>
+                <input
+                  type="file"
+                  name="file"
+                  id="file"
+                  className={cx("card-create__input-file")}
+                  onChange={handleChangeFile}
+                  accept="image/*"
                 />
-              </Form.Item>
-            </div> */}
-          </div>
-
-          <div className={cx("card-create__right")}>
-            <img className={cx("card-create__banner")} />
-            <div className={cx("card-create__import-file")}>
-              <label htmlFor="file">
-                <UploadOutlined />
-                Choose a file
-              </label>
-              <input
-                type="file"
-                name="file"
-                id="file"
-                className={cx("card-create__input-file")}
-              />
+              </div>
             </div>
           </div>
-        </div>
-        <Button
-          primary
-          className={cx("card-create__btn")}
-        >
-          Create
-        </Button>
-        <Progress />
+          <div className={cx("form__submit")}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={cx("btn__submit")}
+            >
+              Create
+            </Button>
+            <Progress percent={progress} style={{ marginLeft: "10px" }} />
+          </div>
+        </Form>
       </div>
     </div>
   );
-}
+};
 
 export default CreateCourse;
